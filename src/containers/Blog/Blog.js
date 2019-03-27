f3a7db5473d2003fe1cb62120041f6842cdbbb9f
KrystianKaminski <krystiankaminski07@gmail.com>
2019-03-27 14:08:41 +0100

    Handling with errors

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -10,11 +10,12 @@ class Blog extends Component {
 
     state = {
         posts: [],
-        selectedPostId: null
+        selectedPostId: null,
+        error: false
     }
 
     componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get('https://jsonplaceholder.typicode.com/postss')
             .then(response => {
                 const posts = response.data.slice(0, 4)
                 const updatedPosts = posts.map(post => {
@@ -27,6 +28,9 @@ class Blog extends Component {
                     posts: updatedPosts
                 })
             })
+            .catch(error => {
+                this.setState({ error: true })
+            })
     }
 
     postSelectedHandler = (id) => {
@@ -36,14 +40,17 @@ class Blog extends Component {
     }
 
     render() {
-        const posts = this.state.posts.map(post => {
-            return <Post
-                key={post.id}
-                title={post.title}
-                author={post.author}
-                clicked={() => this.postSelectedHandler(post.id)}
-            />
-        })
+        let posts = <p style={{ textAlign: 'center' }}>Something went wrong!</p>
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post
+                    key={post.id}
+                    title={post.title}
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)}
+                />
+            })
+        }
         return (
             <div>
                 <section className="Posts">
